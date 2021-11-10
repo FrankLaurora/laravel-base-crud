e1360d673f16ed3b0c04ff5d84661c5a587c2496
FrankLaurora <francesco.laurora9@gmail.com>
2021-11-10 19:27:27 +0100

    Add container to show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,10 +1,12 @@
 @extends('layout.base')
 
 @section('pageContent')
-    <h1>Titolo: {{$comic['title']}}</h1>
-    <h2>Serie: {{$comic['series']}}</h2>
-    <p><strong>Descrizione:</strong> {{$comic['description']}}</p>
-    <h3>Data d'uscita: {{$comic['sale_date']}}</h3>
-    <h3>Prezzo: {{$comic['price']}}</h3>
-    <h4>Formato: {{$comic['type']}}</h4>
+    <main class="container">
+        <h1>Titolo: {{$comic['title']}}</h1>
+        <h2>Serie: {{$comic['series']}}</h2>
+        <p><strong>Descrizione:</strong> {{$comic['description']}}</p>
+        <h3>Data d'uscita: {{$comic['sale_date']}}</h3>
+        <h3>Prezzo: {{$comic['price']}}</h3>
+        <h4>Formato: {{$comic['type']}}</h4>
+    </main>
 @endsection
